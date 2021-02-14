3e2db30e07adbaf3e4f1c3ab0292d7786d8f1d2d
noura-osama <77943136+noura-osama@users.noreply.github.com>
2021-02-14 14:14:07 +0200

    sec

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -47,10 +47,24 @@ export class ProfileComponent implements OnInit {
 
   }
   upload() {
+    this.euser.append('username',this.user.username);
+    this.euser.append('password',this.user.password);
+    this.euser.append('Fname',this.user.Fname);
+    this.euser.append('Lname',this.user.Lname);
+    this.euser.append('email',this.user.email);
+    this.euser.append('dob',this.user.dob);
+    for(let i=0;i<this.user.following.length;i++){
+      this.euser.append('following',this.user.following[i]);
+    }
+    for(let i=0;i<this.user.followers.length;i++){
+      this.euser.append('following',this.user.followers[i]);
+    }
+    console.log(this.user);
     console.log(this.euser)
     this.userService.editUserData(this.euser).subscribe(
       b => {
         console.log(b)
+        location.reload();
       }
     )
   }
